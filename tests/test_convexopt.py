ec468f0195c2bd554000d21266fbf57df0f32cf8
Yonatan Kurniawan <kurniawanyo@outlook.com>
2024-06-27 19:50:47 -0600

    TEST: Update the test for the convex optimization part

--- tests/test_convexopt.py
+++ tests/test_convexopt.py
@@ -12,19 +12,17 @@ fim_target = np.eye(3) * scale
 # Configurations
 fim_config_1 = np.eye(3)
 fim_config_2 = np.diag(np.zeros(3))
-fim_configs_tensor = np.array([fim_config_1, fim_config_2])
-# Config IDs
-config_ids = ["1", "2"]
+fim_configs = {"1": fim_config_1, "2": fim_config_2}
 
 # Solve
-cvxopt = ConvexOpt(fim_target, fim_configs_tensor, config_ids)
+cvxopt = ConvexOpt(fim_target, fim_configs)
 cvxopt.solve()
 
 
-def test_default_norm():
-    assert cvxopt.norm_qoi == 1.0, "Default norm_qoi fail"
-    assert all(cvxopt.norm_conf == np.ones(2)), "Default norm_conf fail"
-    assert all(cvxopt.norm_weights == np.ones(2)), "Default norm_weights fail"
+def test_default_scale():
+    assert cvxopt.scale_qoi == 1.0, "Default scale_qoi fail"
+    assert all(cvxopt.scale_conf == np.ones(2)), "Default scale_conf fail"
+    assert all(cvxopt.scale_weights == np.ones(2)), "Default scale_weights fail"
 
 
 def test_result_keys():
@@ -35,13 +33,13 @@ def test_result_keys():
 def test_optimal_result():
     opt_config = cvxopt.get_config_weights(1e-6)
     # For this simple case, we know which configuration is optimal and its optimal weight
-    assert list(opt_config)[0] == config_ids[0], "Wrong optimal config"
+    assert list(opt_config)[0] == "1", "Wrong optimal config"
     assert np.isclose(
-        opt_config[config_ids[0]], scale, rtol=1e-4, atol=1e-4
+        opt_config["1"], scale, rtol=1e-4, atol=1e-4
     ), "Wrong optimal weight"
 
 
 if __name__ == "__main__":
-    test_default_norm()
+    test_default_scale()
     test_result_keys()
     test_optimal_result()
