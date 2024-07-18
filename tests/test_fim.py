53f330744bb29481b2f83bbe3f5b4713d33d85d6
Yonatan Kurniawan <kurniawanyo@outlook.com>
2024-07-17 21:11:09 -0600

    I drafted the test for FIM jl
    Issue: I am still using PyCall and I cannot make pytest to work
    with PyCall. A possible solution, although not tried yet, is to
    use the newer PythonCall.

--- tests/test_fim.py
+++ tests/test_fim.py
@@ -5,6 +5,14 @@ from information_matching.fim import FIM_fd
 from information_matching.fim import FIM_nd
 from information_matching.fim.fim_fd import CD
 
+# try:
+#     from information_matching.fim.fim_jl import FIM_jl
+
+#     julia_avail = True
+# except ImportError:
+#     print("Skipping testing FIM module that uses Julia")
+#     julia_avail = False
+
 
 xlist = np.random.uniform(0, 1, 10)
 tlist = np.random.uniform(0, 1, 10)
@@ -45,24 +53,21 @@ def test_evaluation_nd():
     fim_fn = FIM_nd(fn)
     jac_nd = fim_fn.compute_jacobian(xlist)
     # Compare the jacobian
-    assert np.all(np.abs(jac_truth - jac_nd) < 1e-8)
+    assert np.all(np.diag(jac_nd - jac_truth) / np.diag(jac_truth) < 1e-8)
     # Compare the FIM
     fim_nd = fim_fn.compute_FIM(xlist)
-    assert np.all(np.abs(fim_truth - fim_nd) < 1e-6)
+    assert np.all(np.diag(fim_nd - fim_truth) / np.diag(fim_truth) < 1e-8)
 
 
-def test_kwargs_nd():
-    kwargs = dict(step=0.01)  # Keyword argument for nd.Jacobian
-    fim_fn = FIM_nd(fn_kwargs, **kwargs)
-    # Test for exception if function kwargs is not given
-    with pytest.raises(TypeError):
-        _ = fim_fn.compute_jacobian(xlist)
-    # Test the Jacobian value
-    jac_nd = fim_fn.compute_jacobian(xlist, tlist)
-    assert np.allclose(jac_nd, jac_kwargs_truth, atol=1e-4, rtol=1e-4)
-    # Test the FIM value
-    fim_nd = fim_fn(xlist, tlist)
-    assert np.allclose(fim_nd, fim_kwargs_truth, atol=1e-4, rtol=1e-4)
+# def test_evaluation_jl():
+#     if julia_avail:
+#         fim_fn = FIM_jl(fn)
+#         jac_jl = fim_fn.compute_jacobian(xlist)
+#         # Compare the jacobian
+#         assert np.all(np.diag(jac_jl - jac_truth) / np.diag(jac_truth) < 1e-8)
+#         # Compare the FIM
+#         fim_jl = fim_fn.compute_FIM(xlist)
+#         assert np.all(np.diag(fim_jl - fim_truth) / np.diag(fim_truth) < 1e-8)
 
 
 def test_kwargs_fd():
@@ -81,8 +86,39 @@ def test_kwargs_fd():
     assert np.all(np.abs((fim_fd - fim_kwargs_truth) / fim_kwargs_truth) < h)
 
 
+def test_kwargs_nd():
+    kwargs = dict(step=0.01)  # Keyword argument for nd.Jacobian
+    fim_fn = FIM_nd(fn_kwargs, **kwargs)
+    # Test for exception if function kwargs is not given
+    with pytest.raises(TypeError):
+        _ = fim_fn.compute_jacobian(xlist)
+    # Test the Jacobian value
+    jac_nd = fim_fn.compute_jacobian(xlist, tlist)
+    assert np.allclose(jac_nd, jac_kwargs_truth, atol=1e-4, rtol=1e-4)
+    # Test the FIM value
+    fim_nd = fim_fn(xlist, tlist)
+    assert np.allclose(fim_nd, fim_kwargs_truth, atol=1e-4, rtol=1e-4)
+
+
+# def test_kwargs_jl():
+#     if julia_avail:
+#         kwargs = dict(h=0.01)  # Keyword argument for Julia's Numdifftools.jacobian
+#         fim_fn = FIM_jl(fn_kwargs, **kwargs)
+#         # Test for exception if function kwargs is not given
+#         with pytest.raises(TypeError):
+#             _ = fim_fn.compute_jacobian(xlist)
+#         # Test the Jacobian value
+#         jac_jl = fim_fn.compute_jacobian(xlist, tlist)
+#         assert np.allclose(jac_jl, jac_kwargs_truth, atol=1e-4, rtol=1e-4)
+#         # Test the FIM value
+#         fim_jl = fim_fn(xlist, tlist)
+#         assert np.allclose(fim_jl, fim_kwargs_truth, atol=1e-4, rtol=1e-4)
+
+
 if __name__ == "__main__":
     test_evaluation_fd()
     test_evaluation_nd()
-    test_kwargs_nd()
+    # test_evaluation_jl()
     test_kwargs_fd()
+    test_kwargs_nd()
+    # test_kwargs_jl()
