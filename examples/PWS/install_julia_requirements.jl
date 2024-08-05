d4a8e2f03a521e339f41a38081224dfa7bbb7c98
yonatank93 <kurniawanyo@outlook.com>
2024-08-04 21:46:27 -0600

    Add Modeling julia package to requirements
    This package is apparently used by the IEEE models that Ben sent me.

--- examples/PWS/install_julia_requirements.jl
+++ examples/PWS/install_julia_requirements.jl
@@ -4,3 +4,6 @@ import Pkg
 # Import registered packages
 Pkg.add("PyCall")
 Pkg.add(Pkg.PackageSpec(url="https://github.com/mktranstrum/NumDiffTools.jl"))
+
+#Install Main Modeling Package
+Pkg.add(Pkg.PackageSpec(url="https://git.physics.byu.edu/Modeling/Models.jl.git"))
