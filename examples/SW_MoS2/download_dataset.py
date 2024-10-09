6c3c65341306966bd481a63d95b8f2543bd1c834
yonatank93 <kurniawanyo@outlook.com>
2024-10-09 07:31:04 -0600

    Use the new download_dataset function

--- examples/SW_MoS2/download_dataset.py
+++ examples/SW_MoS2/download_dataset.py
@@ -1,36 +1,17 @@
 """Run this script to download the dataset for the SW MoS2 example."""
 
 from pathlib import Path
-import requests
-import tarfile
-
+from information_matching.utils import download_dataset
 
 FILE_PATH = Path(__file__).resolve().parent
 
-# Check if the dataset is already downloaded and extracted
-example_name = "SW_MoS2"
-dataset_path = FILE_PATH / "sw_mos2_training_dataset"
+# Dictionary with the dataset information
+dataset_info = {
+    "dataset_path": FILE_PATH / "sw_mos2_training_dataset",
+    "url": "https://raw.githubusercontent.com/yonatank93/information-matching/"
+    "main/examples/SW_MoS2/sw_mos2_training_dataset.tar.gz",
+    "tar_path": FILE_PATH / "sw_mos2_training_dataset.tar.gz",
+}
 
-if not dataset_path.exists():
-    # Check if the tar.gz file is already downloaded
-    tar_path = dataset_path.with_suffix(".tar.gz")
-
-    if not tar_path.exists():
-        # Prepare the url
-        url = (
-            "https://raw.githubusercontent.com/yonatank93/information-matching/main/examples/"
-            f"{example_name}/{dataset_path.name}.tar.gz"
-        )
-
-        # Download the dataset
-        print(f"Downloading dataset from {url}")
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(tar_path, "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-
-    # Extract the dataset
-    print(f"Extracting dataset to {dataset_path}")
-    tf = tarfile.open(tar_path, "r:gz")
-    tf.extractall(FILE_PATH)
+# Download the dataset
+download_dataset(dataset_info)
