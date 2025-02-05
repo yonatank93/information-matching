d4eb6d32a93afdd20fc315243015907a9ffe012c
yonatank93 <kurniawanyo@outlook.com>
2025-02-04 21:23:26 -0700

    Convert the documentation to numpy style.

--- information_matching/transform.py
+++ information_matching/transform.py
@@ -1,52 +1,57 @@
 """Collection of parameter transformations."""
 
 from abc import ABC, abstractmethod
-from typing import Optional, Union
 import numpy as np
 
 
 class TransformBase(ABC):
-    """
-    Abstract base class for parameter transformations
-    """
+    """Abstract base class for parameter transformations."""
 
     def __init__(self, **kwargs):
         super().__init__()
         self._kwargs = kwargs
 
     @abstractmethod
-    def transform(self, x: np.ndarray) -> np.ndarray:
-        """
-        Transform parameters from the original parameterization to the
-        transformed parameterization to work with.
+    def transform(self, x):
+        """Transform parameters from the original parameterization to the transformed
+        parameterization to work with.
 
-        :param x: parameter values in the original parameterization
-        :type x: np.ndarray
+        Parameters
+        ----------
+        x: np.ndarray
+            Parameter values in the original parameterization.
 
-        :returns: parameter values in the transformed parameterization
-        :rtype: np.ndarray
+        Returns
+        -------
+        np.ndarray
+            Parameter values in the transformed parameterization.
         """
         raise NotImplementedError
 
     @abstractmethod
-    def inverse_transform(self, x: np.ndarray) -> np.ndarray:
-        """
-        Transform parameters from the transformed parameterization to the
-        original parameterization to work with.
+    def inverse_transform(self, x):
+        """Transform parameters from the transformed parameterization to the original
+        parameterization to work with.
 
-        :param x: parameter values in the transformed parameterization
-        :type x: np.ndarray
+        Parameters
+        ----------
+        x: np.ndarray
+            Parameter values in the transformed parameterization.
 
-        :returns: parameter values in the original parameterization
-        :rtype: np.ndarray
+        Returns
+        -------
+        np.ndarray
+            Parameter values in the original parameterization.
         """
         raise NotImplementedError
 
     @property
-    def jsonable_kwargs(self) -> dict:
-        """
-        Convert the keyword arguments used when initializing the class to a
-        JSON serializable dictionary
+    def jsonable_kwargs(self):
+        """Convert the keyword arguments used when initializing the class to a JSON
+        serializable dictionary.
+
+        This is mainly used if other code need to save the transformation information
+        as a metadata to a JSON file.
         """
         kwargs = self._kwargs.copy()
         for key, val in kwargs.items():
@@ -54,32 +59,27 @@ class TransformBase(ABC):
                 kwargs[key] = val.tolist()
         return kwargs
 
-    def __call__(self, x: np.ndarray) -> np.ndarray:
+    def __call__(self, x):
         return self.transform(x)
 
 
 class AffineTransform(TransformBase):
+    """An affine transformation class :math:`y = A.x + b`.
+
+    Parameters
+    ----------
+    A: float or np.ndarray
+        2d array or a float.
+
+    b: float or np.ndarray
+        1d array or a float.
+
+    Ainv: None or float or np.ndarray
+        Inverse of A. If None is given, inverse will be computed by taking reciprocal
+        of a if a is a float, otherwise using `np.linalg.pinv(a)`.
     """
-    An affine transformation class :math:`y = A.x + b`.
 
-    :param A: 2d array or a float
-    :type A: float or np.ndarray
-
-    :param b: 1d array or a float
-    :type : float or np.ndarray
-
-    :param Ainv: Inverse of A. If None is given, inverse will be computed by
-        taking reciprocal of a if a is a float, otherwise using
-        `np.linalg.pinv(a)`.
-    :type: None or float or np.ndarray
-    """
-
-    def __init__(
-        self,
-        a: Optional[Union[float, np.ndarray]] = 1.0,
-        b: Optional[Union[float, np.ndarray]] = 0.0,
-        ainv: Optional[Union[None, float, np.ndarray]] = None,
-    ):
+    def __init__(self, a=1.0, b=0.0, ainv=None):
         super().__init__(a=a, b=b, ainv=ainv)
         self.a = a
         if isinstance(self.a, (float, int)):
@@ -101,82 +101,91 @@ class AffineTransform(TransformBase):
 
         super().__init__(a=self.a, b=self.b, ainv=self.ainv)
 
-    def transform(self, x: np.ndarray) -> np.ndarray:
-        """
-        Perform parameter transformation to the transformed space.
+    def transform(self, x):
+        """Perform parameter transformation to the transformed space.
 
-        :param x: parameter values to transform
-        :type x: np.ndarray
+        Parameters
+        ----------
+        x: np.ndarray
+            Parameter values to transform.
 
-        :returns: parameter values in affine transformed space.
-        :rtype: np.ndarray
+        Returns
+        -------
+        np.ndarray
+            Parameter values in affine transformed space.
         """
         return self._mult_fn(self.a, x) + self.b
 
-    def inverse_transform(self, x: np.ndarray) -> np.ndarray:
-        """
-        Invert the transformation back to the original space.
+    def inverse_transform(self, x):
+        """Invert the transformation back to the original space.
 
-        :param x: parameter values to inverse transform
-        :type x: np.ndarray
+        Parameters
+        ----------
+        x: np.ndarray
+            Parameter values to inverse transform.
 
-        :returns: parameter values in the original parameterization
-        :rtype: np.ndarray
+        Returns
+        -------
+        np.ndarray
+            Parameter values in the original parameterization.
         """
         return self._mult_fn(self.ainv, (x - self.b))
 
     @staticmethod
-    def _scalar_mult(a: float, b: float) -> float:
-        """
-        Multiplication function when the arguments are scalar.
-        """
+    def _scalar_mult(a, b):
+        """Multiplication function when the arguments are scalar."""
         return a * b
 
     @staticmethod
-    def _matrix_mult(a: np.ndarray, b: np.ndarray) -> np.ndarray:
-        """
-        Multiplication function when the arguments are vectors or matrices.
-        """
+    def _matrix_mult(a, b):
+        """Multiplication function when the arguments are vectors or matrices."""
         return a @ b
 
 
 class LogTransform(TransformBase):
-    """
-    An logarithmic transformation base e class :math:`y = log(|x|)`.
-    This transformation preserves the sign, and the sign needs to be specified
-    as an input argument.
+    """An logarithmic transformation base e class :math:`y = log(|x|)`.
 
-    :param sign: sign of the parameter values, which is used to invert the
-        transformation. Make sure that the element values are either 1 or -1
-        only.
-    :type sign: np.ndarray
+    This transformation preserves the sign, and the sign needs to be specified as an
+    input argument.
+
+    Parameters
+    ----------
+    sign: np.ndarray
+        Sign of the parameter values, which is used to invert the transformation. Make
+        sure that the element values are either 1 or -1 only.
     """
 
-    def __init__(self, sign: np.ndarray):
+    def __init__(self, sign):
         self.sign = sign
         super().__init__(sign=self.sign)
 
-    def transform(self, x: np.ndarray) -> np.ndarray:
-        """
-        Perform parameter transformation to the transformed space.
+    def transform(self, x):
+        """Perform parameter transformation to the transformed space.
 
-        :param x: parameter values to transform
-        :type x: np.ndarray
+        Parameters
+        ----------
+        x: np.ndarray
+            Parameter values to transform.
 
-        :returns: parameter values in logarithmically transformed space
-        :rtype: np.ndarray
+        Returns
+        -------
+        np.ndarray
+            Parameter values in logarithmically transformed space.
         """
         return np.log(np.abs(x))
 
-    def inverse_transform(self, x: np.ndarray) -> np.ndarray:
-        """
-        Invert the transformation back to the original space.
+    def inverse_transform(self, x):
+        """Invert the transformation back to the original space.
 
-        :param x: parameter values to inverse transform
-        :type x: np.ndarray
+        Parameters
+        ----------
+        x: np.ndarray
+            Parameter values to inverse transform.
 
-        :returns: parameter values in the original parameterization
-        :rtype: np.ndarray
+        Returns
+        -------
+        np.ndarray
+            Parameter values in the original parameterization.
         """
         return self.sign * np.exp(x)
 
