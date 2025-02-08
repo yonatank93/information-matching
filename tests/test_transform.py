601ade80ddd6f136540efb26474e7df7f1e4cac7
yonatank93 <kurniawanyo@outlook.com>
2025-02-08 16:58:22 -0700

    Update the test in the following ways:
    * Add test for ComposedTransform and AtanhTransform.
    * Add a test that check if some transformations satisfy their constraints.

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -5,24 +5,34 @@ from information_matching.transform import (
     func_wrapper,
     AffineTransform,
     LogTransform,
+    AtanhTransform,
     SplitTransform,
     ComposedTransform,
 )
 
 np.random.seed(1)
+tol = np.finfo(float).eps ** 0.5
 
 # Test parameter value
-x = np.random.randn(3)
+nparams = np.random.randint(1, 10)
+x = np.random.randn(nparams)
 
 # Instantiate the Affine parameter transform
-A = np.random.randn(3, 3)
-x0 = np.random.randn(3)
-b = np.random.randn(3)
+A = np.random.randn(nparams, nparams)
+x0 = np.random.randn(nparams)
+b = np.random.randn(nparams)
 transform_affine = AffineTransform(A, x0, b)
 
 # Instantiate the Log parameter transform
 sign = np.sign(x)
 transform_log = LogTransform(sign)
+
+# Instantiate the Archtanh parameter transform
+midpoint = x + 0.1 * np.random.randn(nparams)
+half_range = np.random.randint(1, 5, nparams)
+low = midpoint - half_range
+high = midpoint + half_range
+transform_atanh = AtanhTransform(low, high)
 
 # Instantiate the Split parameter transform
 transforms_list = [transform_log, transform_affine]
@@ -40,10 +50,36 @@ def test_transform():
     assert np.allclose(transform_affine(x), A @ (x - x0) + b)
     # Test log transformation by manual calculation
     assert np.allclose(transform_log(x), np.log(np.abs(x)))
+    # Test arctanh transformation by manual calculation
+    assert np.allclose(transform_atanh(x), np.arctanh((x - midpoint) / half_range))
     # Test split transformation by manual calculation
     assert np.allclose(
         transform_split(xlong), np.append(np.log(np.abs(x)), A @ (x - x0) + b)
     )
+    # Test composed transformation by manual calculation
+    assert np.allclose(transform_composed(x), A @ (np.log(np.abs(x)) - x0) + b)
+
+
+def test_transform2():
+    """These tests are to check if transfomations that are motivated by certain
+    constraints really satisfy those constraints. For example, the log transformation
+    should return positive values for positive input values and negative values for
+    negative input values.
+    """
+    # Generate random parameter values for testing --- Don't make the range too large
+    # so that the test won't be affected by numerical precision.
+    test_values = np.random.uniform(-1e2, 1e2, (100, nparams))
+
+    # Test if log transformation preserves the sign of the input values
+    for val in test_values:
+        test_val = transform_log.inverse_transform(val)
+        assert np.all(np.sign(test_val) == sign)
+    # Test if arctanh transformation really works within a range
+    for val in test_values:
+        # Invert transformation to see if all numbers are mapped within a range
+        test_val = transform_atanh.inverse_transform(val)
+        # We need to give some tolerance to the range because of the numerical precision
+        assert np.all(test_val >= low - tol) and np.all(test_val <= high + tol)
 
 
 def test_inverse_transform():
@@ -52,8 +88,12 @@ def test_inverse_transform():
     assert np.allclose(transform_affine.inverse_transform(transform_affine(x)), x)
     # Test if log inverse transformation gives back the original parameter values
     assert np.allclose(transform_log.inverse_transform(transform_log(x)), x)
+    # Test if arctanh inverse transformation gives back the original parameter values
+    assert np.allclose(transform_atanh.inverse_transform(transform_atanh(x)), x)
     # Test if split inverse transformation gives back the original parameter values
     assert np.allclose(transform_split.inverse_transform(transform_split(xlong)), xlong)
+    # Test if composed inverse transformation gives back the original parameter values
+    assert np.allclose(transform_composed.inverse_transform(transform_composed(x)), x)
 
 
 def test_func_wrapper():
@@ -72,5 +112,6 @@ def test_func_wrapper():
 
 if __name__ == "__main__":
     test_transform()
+    test_transform2()
     test_inverse_transform()
     test_func_wrapper()
